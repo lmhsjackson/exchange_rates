cacf8c99aab306c60559d693d824e61e6cbe8ca1
Jose Piccioni <josepiccioni@gmail.com>
2018-03-15 21:16:13 -0300

    Add test scaffold for rate model.

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Rate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :rates
+
+  describe '.at' do
+    context 'date is present or past' do
+      let(:date) { Date.today }
+
+      context 'currencies are valid' do
+        it '' do
+          # expect
+        end
+      end
+
+      context 'currencies are not supported/invalid' do
+
+      end
+    end
+
+    context 'date is in the future' do
+      it '' do
+        # expect
+      end
+    end
+  end
 end
