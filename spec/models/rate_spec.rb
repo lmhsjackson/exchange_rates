4ade453f7a914ad19452efaec4ae0694ab50748b
Jose Piccioni <josepiccioni@gmail.com>
2018-03-15 21:48:18 -0300

    Rate .at method and specs first take.

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -4,17 +4,31 @@ RSpec.describe Rate, type: :model do
   fixtures :rates
 
   describe '.at' do
+    let(:subject) { Rate }
+
     context 'date is present or past' do
       let(:date) { Date.today }
 
       context 'currencies are valid' do
-        it '' do
-          # expect
+        let(:base_currency) { 'SEK' }
+        let(:counter_currency) { 'EUR' }
+
+        it 'performs a where operation with given params' do
+          allow(subject).to receive(:where).and_call_original
+          rate = subject.at(date, base_currency, counter_currency)
+          expect(subject).to have_received(:where).with(date: date, base_currency: base_currency, counter_currency: counter_currency)
+        end
+
+        it 'returns the expected rate' do
+          expected_rate = subject.where(date: date, base_currency: base_currency, counter_currency: counter_currency).first.rate
+          rate = subject.at(date, base_currency, counter_currency)
+          expect(rate).to equal(expected_rate)
         end
       end
 
       context 'currencies are not supported/invalid' do
-
+        let(:base_currency) { 'broken' }
+        let(:counter_currency) { 'EUR' }
       end
     end
 
