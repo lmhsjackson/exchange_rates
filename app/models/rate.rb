3034ee4773ddc8bb725420033807b9f6084c6fbe
Jose Piccioni <josepiccioni@gmail.com>
2018-03-16 11:31:06 -0300

    Exception handling and last unit tests.

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,7 +1,15 @@
 class Rate < ActiveRecord::Base
+  VALID_CURRENCY = ['SEK', 'NOK', 'EUR']
+
   def self.at(date, base_currency, counter_currency)
     return 1 if base_currency == counter_currency
-    Rate.where(date: Date.parse(date), base_currency: base_currency,
-               counter_currency: counter_currency).first.rate
+
+    date = Date.parse date
+    raise ArgumentError, 'Invalid date' if Date.today < date
+    raise ArgumentError, 'Invalid currency' unless(VALID_CURRENCY.include?(base_currency) && VALID_CURRENCY.include?(counter_currency))
+    search_result = Rate.where(date: date, base_currency: base_currency,
+               counter_currency: counter_currency)
+    raise ActiveRecord::RecordNotFound, 'no exchange rate was found for the data provided' if search_result.empty?
+    search_result.first.rate
   end
 end
--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -4,37 +4,54 @@ RSpec.describe Rate, type: :model do
   fixtures :rates
 
   describe '.at' do
-    let(:subject) { Rate }
+    let(:base_currency) { 'SEK' }
+    let(:counter_currency) { 'EUR' }
+    let(:date) { Date.today.to_s }
 
     context 'date is present or past' do
-      let(:date) { Date.today }
-
-      context 'currencies are valid' do
-        let(:base_currency) { 'SEK' }
-        let(:counter_currency) { 'EUR' }
-
-        it 'performs a where operation with given params' do
-          allow(subject).to receive(:where).and_call_original
-          rate = subject.at(date, base_currency, counter_currency)
-          expect(subject).to have_received(:where).with(date: date, base_currency: base_currency, counter_currency: counter_currency)
-        end
-
-        it 'returns the expected rate' do
-          expected_rate = subject.where(date: date, base_currency: base_currency, counter_currency: counter_currency).first.rate
-          rate = subject.at(date, base_currency, counter_currency)
-          expect(rate).to equal(expected_rate)
-        end
+      it 'performs a where operation with given params' do
+        allow(Rate).to receive(:where).and_call_original
+        parsed_date = Date.parse(date)
+        rate = Rate.at(date, base_currency, counter_currency)
+        expect(Rate).to have_received(:where).with(date: parsed_date,
+                                                   base_currency: base_currency,
+                                                   counter_currency: counter_currency)
       end
 
-      context 'currencies are not supported/invalid' do
-        let(:base_currency) { 'broken' }
-        let(:counter_currency) { 'EUR' }
+      it 'returns the expected rate' do
+        expected_rate = Rate.where(date: date,
+                                   base_currency: base_currency,
+                                   counter_currency: counter_currency).first.rate
+        rate = Rate.at(date, base_currency, counter_currency)
+        expect(rate).to equal(expected_rate)
+      end
+    end
+
+    context 'currencies are not supported/invalid' do
+      let(:base_currency) { 'broken' }
+      let(:counter_currency) { 'EUR' }
+      it 'sends an error saying so' do
+        expect do
+          Rate.at(date, base_currency, counter_currency)
+        end.to raise_error(ArgumentError, 'Invalid currency')
+      end
+    end
+
+    context 'No data found' do
+      let(:date) { Date.yesterday.to_s }
+      it 'sends an error saying so' do
+        expect do
+          Rate.at(date, base_currency, counter_currency)
+        end.to raise_error(ActiveRecord::RecordNotFound, 'no exchange rate was found for the data provided')
       end
     end
 
     context 'date is in the future' do
-      it '' do
-        # expect
+      let(:date) { Date.tomorrow.to_s }
+      it 'sends an error saying so' do
+        expect do
+          Rate.at(date, base_currency, counter_currency)
+        end.to raise_error(ArgumentError, 'Invalid date')
       end
     end
   end
