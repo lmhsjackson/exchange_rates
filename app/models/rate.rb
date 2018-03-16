137da831055935e853c471da40135d8641c774e2
Jose Piccioni <josepiccioni@gmail.com>
2018-03-15 22:32:25 -0300

    Parse date string before using it in where clause.

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,6 +1,6 @@
 class Rate < ActiveRecord::Base
   def self.at(date, base_currency, counter_currency)
-    Rate.where(date: date, base_currency: base_currency,
+    Rate.where(date: Date.parse(date), base_currency: base_currency,
                counter_currency: counter_currency).first.rate
   end
 end
